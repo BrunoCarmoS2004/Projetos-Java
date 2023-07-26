8adfdfdbf7a8e588e601e7de0b8b2c37a8bfd9c7
BrunoCarmoS2004 <126525394+BrunoCarmoS2004@users.noreply.github.com>
2023-07-26 19:57:20 -0300

    Update PegarNumeros.java

--- ProjetosJava/Calculadora/PegarNumeros.java
+++ ProjetosJava/Calculadora/PegarNumeros.java
@@ -9,22 +9,21 @@ public class PegarNumeros {
         int i = 0;
         Scanner scanner = new Scanner(System.in);
     public void numeros(){
-        //colocar o while aqui
-        for (i = 0; i < vetorNumeros.length; i++) {
-            vetorNumeros = new float[num];
-            while (resp == 1){
-            System.out.printf("Escreva o %d° numero: ",num);
-            vetorNumeros[i] = scanner.nextFloat();
-            System.out.println("Deseja adicionar mais um numero? ");
-            System.out.println("1 - SIM || 2 - NÃO");
-            resp = scanner.nextInt();
-            if (resp == 2 && num ==1){
-                System.out.println("Tem que adicionar pelo menos mais um numero!");
-                resp = 1;
+        vetorNumeros = new float[num];
+        while (resp == 1){
+            for (i = 0; i < vetorNumeros.length; i++){
+                System.out.printf("Escreva o %d° numero: ",num);
+                vetorNumeros[i] = scanner.nextFloat();
+                System.out.println("Deseja adicionar mais um numero? ");
+                System.out.println("1 - SIM || 2 - NÃO");
+                resp = scanner.nextInt();
+                if (resp == 2 && num ==1){
+                    System.out.println("Tem que adicionar pelo menos mais um numero!");
+                    resp = 1;
+                }
+                
             }
-            num++;
-            }
-            
+                num++;
         }
         
     }
