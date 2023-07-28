bb941e1eaf08ed6a6baa84ee8d88f326f8572b71
BrunoCarmoS2004 <126525394+BrunoCarmoS2004@users.noreply.github.com>
2023-07-27 22:03:01 -0300

    xzcxcv

--- ProjetosJava/Calculadora/PegarNumeros.java
+++ ProjetosJava/Calculadora/PegarNumeros.java
@@ -9,24 +9,21 @@ public class PegarNumeros {
     ArrayList<Float> banana;
     public void numeros(){
 
-        // Create an ArrayList to store the numbers
+        
         ArrayList<Float> numbers = new ArrayList<>();
 
-        // Prompt the user to enter numbers
+        
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite qual quer numero. Digite apenas 0 para terminar\n");
 
-        // Loop until the user enters 0
+        
         while (true) {
-            // Get the user input
             System.out.printf("Digite o %d° numero: ",num);
             number = scanner.nextFloat();
             num++;
-            // If the user enters 0, exit the loop
             if (number == 0) {
                 break;
             }
-            // Add the number to the list
             banana = numbers;
             banana.add(number);
             
