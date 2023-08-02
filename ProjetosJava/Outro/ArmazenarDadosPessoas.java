ad41de8ec6a9e0552634fc8f727575f32049f37d
BrunoCarmoS2004 <126525394+BrunoCarmoS2004@users.noreply.github.com>
2023-08-02 17:44:09 -0300

    dassfdghfj

--- ProjetosJava/Outro/ArmazenarDadosPessoas.java
+++ ProjetosJava/Outro/ArmazenarDadosPessoas.java
@@ -5,7 +5,11 @@ import java.util.ArrayList;
 public class ArmazenarDadosPessoas {
     public void info(ArrayList<String>palavras, ArrayList<Integer>numeros){
         for (int i = 0; i < palavras.size(); i++) {
-            System.out.printf("O nome da pessoa é %d",i-1);
+            System.out.printf("O nome da pessoa é %d\n",palavras.get(i));
+            System.out.printf("Com o sobre nome %d\n",palavras.get(i+1));
+            System.out.printf("De pele %d\n",palavras.get(i+2));
+            System.out.printf("Se considerando %d\n",palavras.get(i+3));
+            System.out.printf("Com %d de idade\n",palavras.get(i+4));
         }
     }
 }
--- ProjetosJava/Outro/CadastroPessoas.java
+++ ProjetosJava/Outro/CadastroPessoas.java
@@ -9,39 +9,43 @@ public class CadastroPessoas {
         ArrayList<String>palavras = new ArrayList<>();
         ArrayList<Integer>numeros = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        String nome, cor, sobreN, sexo;
+        String nome = "";
+        String sobreN = "";
+        String cor = "";
+        String sexo = "";
         int idade = 0;
         int num = 1;
-        while (num <= 5){
-            if (num ==1){
+        System.out.println("Se quiser finalizar digite apenas 0");
+        while (num != 3){
+            
+           
                 System.out.print("Escreva o nome da pessoa: ");
                 nome = scanner.next();
                 palavras.add(nome);      
-                num++;  
-            }else if(num == 2){
-                System.out.print("Escreva o sobre nome da pessoa: ");
+                
+            
+                System.out.print("Agora o SOBRE NOME da pessoa: ");
                 sobreN = scanner.next();
                 palavras.add(sobreN);      
-                num++; 
-            }else if(num == 3){
+            
+            
                 System.out.print("Escreva a cor da pessoa: ");
                 cor = scanner.next();
                 palavras.add(cor);      
-                num++; 
-            }else if(num == 4){
+     
+          
                 System.out.print("Escreva o sexo/genrero da pessoa: ");
                 sexo = scanner.next();
                 palavras.add(sexo);      
-                num++; 
-            }else if(num == 5){
+ 
+           
                 System.out.print("Escreva a idade da pessoa: ");
                 idade = scanner.nextInt();
                 numeros.add(idade);      
-                num++; 
-            }
+                num++;  
+            
         }
-
-        
+        armazen.info(palavras, numeros);
     }
 }
 
