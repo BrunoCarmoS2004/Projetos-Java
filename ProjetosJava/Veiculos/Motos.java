060c86eb98d8687b495a2c2cce83e7dc2f1c3f56
BrunoCarmoS2004 <126525394+BrunoCarmoS2004@users.noreply.github.com>
2023-08-09 16:13:19 -0300

    Criando as Classes

---
+++ ProjetosJava/Veiculos/Motos.java
@@ -0,0 +1,5 @@
+package ProjetosJava.Veiculos;
+
+public class Motos {
+    
+}
--- ProjetosJava/Veiculos/TelaMainVeic.java
+++ ProjetosJava/Veiculos/TelaMainVeic.java
@@ -6,22 +6,41 @@ public class TelaMainVeic {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num1 = 5;
-        int escolha1 =5;
+        int escolha1 = 5;
+        int escolha2 = 5;
         System.out.println("Bem vindo a garagem C-137");
         System.out.println("Aqui você poderam adicionar, modificar, excluir e apostar corrida com carros, motos, aviões ou barcos!");
         System.out.println("Escolha uma das opções da tebela abaixo");
-        while (escolha1 < 1 || escolha1 > 3){
+        while (escolha1 !=0 ){
             System.out.println("---------------------------"
             +"1 - Carro"
             +"2 - Motos"
-            +"2 - Avião"
-            +"3 - Barco");
+            +"3 - Avião"
+            +"4 - Barco"
+            +"0 - Sair");
             escolha1 = scanner.nextInt();
-        }
-
-        switch(escolha1){
-            case 1:
-            System.out.println("asaa");
+            switch(escolha1){
+                case 1:
+                while(true){
+                    System.out.println("1 - Adicionar Carro"
+                    +"2 - Modificar Carro"
+                    +"3 - Excluir Carro"
+                    +"4 - Apostar Corrida entre os carros"
+                    +"0 - Voltar");
+                    escolha2 = scanner.nextInt();
+                    if(escolha2 == 1){
+                        //Adicionar carros;
+                    }else if(escolha1 == 2){
+                        //Modificar carro
+                    }else if(escolha1 == 3){
+                        //Excluir carro
+                    }else if(escolha1 == 4){
+                        //Corrida
+                    }else if(escolha1 == 0){
+                        break;
+                    }
+                }
+            } 
         }
     }
 }
