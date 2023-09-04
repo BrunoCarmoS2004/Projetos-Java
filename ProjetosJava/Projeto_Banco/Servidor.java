74bc14994ab0f24d788fb2ff9ea0c6c2078a0ae9
BrunoCarmoS2004 <126525394+BrunoCarmoS2004@users.noreply.github.com>
2023-09-04 17:42:30 -0300

    1.1

--- ProjetosJava/Projeto_Banco/Servidor.java
+++ ProjetosJava/Projeto_Banco/Servidor.java
@@ -12,63 +12,62 @@ public class Servidor {
     
     
     public void criarCliente(int numClientes){
-        if (numClientes == 0){
-            System.out.println("Não tem conta?\n");
-            System.out.println("É simples e fácil criar uma!\n");
-            System.out.println("Vamos começar!\n");
-        }
-        System.out.println("Por favor digite o nome COMPLETO:");
-        System.out.print("Sua resposta: ");
-        respNome = scanner.next();
-        System.out.println();
-        System.out.println("Facil né?");
-        System.out.println("Vou precisar de mais algumas informações por segurança\n");
-        System.out.println("Digite seu CPF, não se preocupe suas informações não seram divulgadas");
-        System.out.print("Resposta: ");
-        respCpf = scanner.nextInt();
-        System.out.println();
-        System.out.println("Digite sua data de nascimento");
-        System.out.print("Resposta: ");
-        scanner.next();
-        System.out.println();
-        System.out.println("Digite o nome da sua mãe");
-        System.out.print("Resposta: ");
-        scanner.next();
-        System.out.println("Digite o nome do seu pai");
-        System.out.print("Resposta: ");
-        scanner.next();
-        System.out.println("Por fim digite sua incrivel senha!");
-        while(perfSenha.length()<3){
-            System.out.println("Digite a sua senha");
-            System.out.print("Resposta: ");
-            perfSenha = scanner.next();
-            if (perfSenha.length()<3){
-                System.out.println("Senha muito pequena\n");
-            }else{
-                respSenha = perfSenha;
+            if (numClientes == 0){
+                System.out.println("Não tem conta?\n");
+                System.out.println("É simples e fácil criar uma!\n");
+                System.out.println("Vamos começar!\n");
             }
-       }
-       System.out.println("Terminamos por aqui...\n");
-       System.out.println("Algumas dessas informações poderam ser mudadas");
-       System.out.println();
-       Cliente cliente = new Cliente(respCpf, respSenha, respNome);
-       cli.add(cliente);
-       
-    }
-    public void mostrarClientes(){
-        System.out.println("Aqui estão os clientes cadastrados");
-        for (Cliente i : cli) {
-            System.out.print("Numero do cliente "+i+ " é "+ c);
+            System.out.println("Por favor digite o nome COMPLETO:");
+            System.out.print("Sua resposta: ");
+            respNome = scanner.next();
             System.out.println();
-            c++;
+            System.out.println("Facil né?");
+            System.out.println("Vou precisar de mais algumas informações por segurança\n");
+            System.out.println("Digite seu CPF, não se preocupe suas informações não seram divulgadas");
+            System.out.print("Resposta: ");
+            respCpf = scanner.nextInt();
+            System.out.println();
+            System.out.println("Digite sua data de nascimento");
+            System.out.print("Resposta: ");
+            scanner.next();
+            System.out.println();
+            System.out.println("Digite o nome da sua mãe");
+            System.out.print("Resposta: ");
+            scanner.next();
+            System.out.println("Digite o nome do seu pai");
+            System.out.print("Resposta: ");
+            scanner.next();
+            System.out.println("Por fim digite sua incrivel senha!");
+            while(perfSenha.length()<3){
+                System.out.println("Digite a sua senha");
+                System.out.print("Resposta: ");
+                perfSenha = scanner.next();
+                if (perfSenha.length()<3){
+                    System.out.println("Senha muito pequena\n");
+                }else{
+                    respSenha = perfSenha;
+                }
+        }
+        System.out.println("Terminamos por aqui...\n");
+        System.out.println("Algumas dessas informações poderam ser mudadas");
+        System.out.println();
+        Cliente cliente = new Cliente(respCpf, respSenha, respNome);
+        cli.add(cliente);
+        
+        }
+    public void mostrarClientes(){
+            System.out.println("Aqui estão os clientes cadastrados");
+            for (Cliente i : cli) {
+                System.out.print("Numero do cliente "+i+ " é "+ c);
+                System.out.println();
+                c++;
 
-        }   
-    }
+            }   
+        }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     public void criarFuncionario(int numClientes){
-       
         System.out.println("Funcionario novo?\n");
         System.out.println("Vamos cadastralo!\n");
         System.out.println("Por favor digite o nome COMPLETO:");
@@ -90,10 +89,22 @@ public class Servidor {
             }else{
                 respSenha = perfSenha;
             }
+        }
+        System.out.println("Agora você é um Funcionario cadastrado...\n");
+        System.out.println("Algumas dessas informações poderam ser mudadas");
+        System.out.println();
+        Funcionario cliente = new Funcionario(respCpf, respSenha, respNome);
     }
-    System.out.println("Agora você é um Funcionario cadastrado...\n");
-    System.out.println("Algumas dessas informações poderam ser mudadas");
-    System.out.println();
-    Funcionario cliente = new Funcionario(respCpf, respSenha, respNome);
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void loginCliente(){
+        
     }
+
+    public void loginFuncionario(){
+        
+    }
+
 }
--- ProjetosJava/Projeto_Banco/Tela_Main.java
+++ ProjetosJava/Projeto_Banco/Tela_Main.java
@@ -1,17 +1,60 @@
 package ProjetosJava.Projeto_Banco;
 
-
+import java.util.Scanner;
 
 public class Tela_Main {
     public static void main(String[] args) {
-        int numClientes = 0;
+        int numClientes = 0, respIncio = 1;
+        Scanner scanner = new Scanner(System.in);
         Servidor cliente = new Servidor();
+        /*
+        
         cliente.criarCliente(numClientes);
         //cliente.mostrarClientes();
         System.out.println(cliente.cli.get(0).getNome());
         cliente.cli.get(0).info();
-       
-        
+        */
+
+        System.out.println("BEM VINDO AO BANCO C-137\n");
+        System.out.println("O que deseja fazer?\n");
+
+        while(respIncio != 0){
+            System.out.println("-----------------------------|\n"
+            +"| 1 - Logar como cliente     |\n"
+            +"------------------------------\n"
+            +"| 2 - Logar como Funcionario |\n"
+            +"------------------------------\n"
+            +"| 3 - Cadastrar cliete       |\n"
+            +"------------------------------\n"
+            +"| 4 - Cadastrar Funcionario  |\n"
+            +"------------------------------\n"
+            +"---------> 0 - SAIR <---------\n");
+            System.out.print("Resposta: ");
+            respIncio = scanner.nextInt();
+            if (respIncio == 0){
+                respIncio = 0;
+            }
+            switch(respIncio){
+                case 1:
+                    //login Cliente
+                break;
+                
+                case 2:
+                    //login Funcionario
+                break;
+    
+                case 3:
+                    cliente.criarCliente(numClientes);
+                    respIncio = 0;
+                break;
+    
+                case 4:
+                    cliente.criarFuncionario(numClientes);
+                    respIncio = 0;
+                break; 
+            }
+        }
+
         
     }
 }
